56b5504ea59b63961d1fb682bd4547aa76966968
Karan Gautam <karangautam.gpm@gmail.com>
2024-05-11 08:03:22 +0530

    handle Reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ document
 document
   .getElementById("decreament")
   .addEventListener("click", handleDecreament);
+
+document.getElementById("resetBtn").addEventListener("click", handleReset);
+
 let counterDisplay = document.getElementById("couter-display");
 let counterValue = 0;
 
@@ -22,4 +25,9 @@ function handleDecreament() {
     counterValue--;
     updateCounterDispaly();
   }
+}
+
+function handleReset() {
+  counterValue = 0;
+  updateCounterDispaly();
 }
